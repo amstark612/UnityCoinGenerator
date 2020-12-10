faf8936dd67116834f378fc5670b8424f0309558
Christina Nguyen <nguy4477@umn.edu>
2020-12-10 11:12:36 -0600

    coins now generate perfectly equidistant

--- Assets/CoinGenerator.cs
+++ Assets/CoinGenerator.cs
@@ -11,14 +11,14 @@ public class CoinGenerator : MonoBehaviour {
 
     private GameObject parent;
     Transform[] cornerPoints;
+    private float offset = 0f;        // for offsetting beginning of paths so all coins are equidistant
 
     internal class Path {
-        internal Vector3 start, direction;
+        internal Vector3 direction;
         internal float angle;
         internal float distance;
 
-        internal Path(Vector3 start, Vector3 direction, float angle, float distance) {
-            this.start = start;
+        internal Path(Vector3 direction, float angle, float distance) {
             this.direction = direction;
             this.angle = angle;
             this.distance = distance;
@@ -32,52 +32,51 @@ public class CoinGenerator : MonoBehaviour {
         // get positions of all children as array
         cornerPoints = GetComponentsInChildren<Transform>();
 
-        // for storing path directions & angles
-        Path[] paths = new Path[cornerPoints.Length - 2];
-
-        // for route planning purposes
-        List<float> distances = new List<float>();
-
         // create new empty GameObject for easier exporting of coins
         parent = new GameObject("Coins");
 
         // skip first transform b/c it's the parent transform
         for (int index = 1; index < cornerPoints.Length - 1; index++) {
-            Transform start = cornerPoints[index];
-            Transform end = cornerPoints[index + 1];
+            Vector3 start = cornerPoints[index].position;
+            Vector3 end = cornerPoints[index + 1].position;
 
-            Vector3 direction = (end.position - start.position).normalized;
+            Vector3 direction = (end - start).normalized;
             float angle = 360 - Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
-            float distance = Vector3.Distance(start.position, end.position);
+            float pathDistance = Vector3.Distance(start, end);
 
-            paths[index - 1] = new Path(start.position, direction, angle, distance);
+            totalLinearDistance += pathDistance;       // for route-planning purposes
 
-            totalLinearDistance += distance;       // for route-planning purposes
+            Vector3 startPosition = start + offset * direction;
 
-
-            // int numCoins = (int)(distance / distanceBetweenCoins) + 1;
-
-            // Transform[] coinPositions = new Transform[numCoins];
-
-            // for (int i = 0; i < numCoins; i++) {
-            //     Vector3 position = start.position + i * distanceBetweenCoins * direction;
-            //     GameObject coin = Instantiate(coinPrefab, position, Quaternion.Euler(0, angle, 0));
-            //     coin.transform.SetParent(parent.transform);
-            // }
-        }
-
-        int totalNumCoins = (int) (totalLinearDistance / distanceBetweenCoins) + 1;
-        float ratio = totalNumCoins / totalLinearDistance;
-
-        foreach (Path path in paths) {
-            GenerateSegmentCoins(ratio, path);
+            GenerateCoinsPerSegment(new Path(direction, angle, pathDistance), startPosition);
         }
 
         // for route planning purposes
         Debug.Log("Distance: " + totalLinearDistance + ", Number of coins: " + parent.transform.childCount);
-        double seconds = totalLinearDistance / 2.5;
+        double seconds = totalLinearDistance / 3.5;
         int min = (int) (seconds / 60);
         Debug.Log("Min time to completion: " + min + " min " + (int) (seconds - min * 60) + " sec");
+    }
+
+    private void GenerateCoinsPerSegment(Path path, Vector3 startPosition) {
+        float distance = offset - distanceBetweenCoins;     // -dist between coins to prevent off by one error (after dropping first coin, distance should = 0)
+
+        int i = 0;
+        while (true) {
+                Vector3 position = startPosition + i * distanceBetweenCoins * path.direction;
+                GameObject coin = Instantiate(coinPrefab, position, Quaternion.Euler(0, path.angle, 0));
+                coin.transform.SetParent(parent.transform);
+
+                distance += distanceBetweenCoins;
+                i++;
+
+                if (distance + distanceBetweenCoins >= path.distance) {
+                    break;
+                }
+        }
+
+        float remainder = path.distance - distance;
+        offset = distanceBetweenCoins - remainder;
     }
 
     private void OnDrawGizmos() {
@@ -92,20 +91,5 @@ public class CoinGenerator : MonoBehaviour {
                 Gizmos.DrawLine(cornerPoints[index].position, cornerPoints[index + 1].position);
             }
         #endif
-    }
-
-    private void GenerateSegmentCoins(float ratio, Path path) {
-        // number of coins for this segment in proportion to segment distance vs total distance
-        int numCoins = (int) System.Math.Round(path.distance * ratio);
-
-        float distBetweenCoins = path.distance / numCoins;
-
-        Debug.Log(distBetweenCoins);
-
-        for (int i = 0; i < numCoins; i++) {
-            Vector3 position = path.start + i * distBetweenCoins * path.direction;
-            GameObject coin = Instantiate(coinPrefab, position, Quaternion.Euler(0, path.angle, 0));
-            coin.transform.SetParent(parent.transform);
-        }
     }
 }
